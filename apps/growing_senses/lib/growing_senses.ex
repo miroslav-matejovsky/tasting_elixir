0beb85c2bb2af6ca1b5cc7c3ea4384f029b30fe7
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-10-04 05:52:43 +0200

    interactivity

--- apps/growing_senses/lib/growing_senses.ex
+++ apps/growing_senses/lib/growing_senses.ex
@@ -10,15 +10,39 @@ defmodule GrowingSenses.Logger do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
+  @doc """
+  Resets the counter to 0.
+  """
+  def reset_counter do
+    GenServer.call(__MODULE__, :reset)
+  end
+
+  @doc """
+  Changes the log message prefix.
+  """
+  def change_message(message) do
+    GenServer.call(__MODULE__, {:change_message, message})
+  end
+
   @impl true
   def init(_opts) do
     schedule_log()
-    {:ok, %{counter: 0}}
+    {:ok, %{counter: 0, message: "Counter:"}}
+  end
+
+  @impl true
+  def handle_call(:reset, _from, state) do
+    {:reply, :ok, %{state | counter: 0}}
+  end
+
+  @impl true
+  def handle_call({:change_message, new_message}, _from, state) do
+    {:reply, :ok, %{state | message: new_message}}
   end
 
   @impl true
   def handle_info(:log, state) do
-    Logger.info("Counter: #{state.counter}")
+    Logger.info("#{state.message} #{state.counter}")
     schedule_log()
     {:noreply, %{state | counter: state.counter + 1}}
   end
